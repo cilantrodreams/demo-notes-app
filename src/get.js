706662b7857451cd2a2e91b2741cfe3d372216ed
Thomas Ngo <thomasngo818@gmail.com>
2022-05-23 10:07:53 -0700

    Add get function implementation

---
+++ src/get.js
@@ -0,0 +1,21 @@
+import handler from "./util/handler";
+import dynamodb from "./util/dynamodb";
+
+export const main = handler(async (event) => {
+  const params = {
+    TableName: process.env.TABLE_NAME,
+    // 'key' defines the partition key and sort key of the item to be retrieved
+    KEY: {
+      userId: "123", // The id of the author
+      noteId: event.pathParameters.id, // The id of the note from the path
+    },
+  };
+
+  const result = await dynamodb.get(params);
+  if (!result.Item) {
+    throw new Error("Item not found.");
+  }
+
+  // return the retrieved item
+  return result.Item;
+});
